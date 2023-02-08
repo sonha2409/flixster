0d4b05c243246041cb6868b7cf3259aa6d4efd2a
Son Ha <sonhathai.2409@gmail.com>
2023-02-08 10:06:18 -0500

    Part 2 done
    Part 2 done

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Son Ha on 2/8/23.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}

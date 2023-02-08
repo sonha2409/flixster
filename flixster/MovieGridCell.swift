81fa1a1b1eed949b50435f57c39fc61f6d122129
Son Ha <sonhathai.2409@gmail.com>
2023-02-07 22:43:21 -0500

    hello
    hello

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,29 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Son Ha on 2/7/23.
+//
+
+import UIKit
+
+class MovieGridCell: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
